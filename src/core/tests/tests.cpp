267319976a2ea6f167bcde9423810554d18dc6e2
amper-xand <173635208+amper-xand@users.noreply.github.com>
2025-06-30 15:29:05 -0600

    feat: simplified tests

--- src/core/tests/tests.cpp
+++ src/core/tests/tests.cpp
@@ -1,75 +1,101 @@
 #include <core/generation.hpp>
 #include <core/representation.hpp>
 
-#include <cstdint>
+#include <algorithm>
 #include <format>
 #include <print>
+#include <ranges>
 #include <string>
 
 namespace core::test {
 
-template <typename Expected>
-class Test {
-   protected:
-    Expected expected;
+std::string message(std::string title, std::string msg, auto expected,
+    auto produced, bool passed) {
+    constexpr int width = 40;
 
-    Test(uint8_t expected) : expected(expected) {}
+    // Format title line with centered title
+    std::string base =
+        std::format("{:=^{}}\n", std::format("==[ {} ]==", title), width);
 
-    struct Result {
-        std::string title, msg;
-        bool passed;
-        Expected expected, produced;
+    std::string status =
+        std::format("{}\n", passed ? "✔ Test Passed" : "✗ Test Failed");
 
-        std::string message() {
-            constexpr int width = 40;
+    std::string user_msg = msg.empty() ? "" : std::format("Info: {}\n", msg);
 
-            // Format title line with centered title
-            std::string base = std::format(
-                "{:=^{}}\n", std::format("==[ {} ]==", title), width);
+    std::string details =
+        std::format("Expected:\n{}\nProduced:\n{}\n", expected, produced);
 
-            std::string status = std::format(
-                "{}\n", passed ? "✔ Test Passed" : "✗ Test Failed");
+    std::string end =
+        std::format("{:=^{}}\n", "", width);  // Bottom bar same width
 
-            std::string user_msg =
-                msg.empty() ? "" : std::format("Info: {}\n", msg);
+    return base + status + user_msg + details + end;
+}
 
-            std::string details = std::format("Expected:\n{}\nProduced:\n{}\n",
-                                              expected, produced);
-
-            std::string end =
-                std::format("{:=^{}}\n", "", width);  // Bottom bar same width
-
-            return base + status + user_msg + details + end;
-        }
+void TestGenerationCount(bool pass_silently) {
+    struct MoveCounts {
+        uint8_t pawns = 0;
     };
 
-   public:
-    virtual Result run() = 0;
-};
+    struct PositionTestCase {
+        std::string fen;
+        MoveCounts moves;
+    };
 
-class PawnGenTest : public Test<uint8_t>, private core::MoveGenerator {
-   public:
-    PawnGenTest(const core::Board& board, uint8_t expected_moves)
-        : core::MoveGenerator(board), Test(expected_moves) {}
+    static const PositionTestCase cases[] = {
+        {.fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR",
+            .moves = {.pawns = 16}},
+        {.fen = "8/8/8/3pP3/8/8/8/8 w", .moves = {.pawns = 1}},
+        {.fen = "8/2p5/8/8/8/8/2P5/8 w", .moves = {.pawns = 2}},
+        {.fen = "8/8/3p4/2pPp3/8/8/8/8 w", .moves = {.pawns = 0}},
+        {.fen = "8/8/8/3P4/3p4/8/8/8 w", .moves = {.pawns = 1}},
+        {.fen = "8/8/8/8/2P1P1P1/8/2P1P1P1/8 w", .moves = {.pawns = 6}},
+        {.fen = "8/8/8/2p1p1p1/2P1P1P1/8/8/8 w", .moves = {.pawns = 0}},
+        {.fen = "8/8/8/3P4/3P4/3P4/3P4/8 w", .moves = {.pawns = 1}},
+        {.fen = "8/8/8/8/8/8/PPPPPPPP/8 w", .moves = {.pawns = 16}},
+        {.fen = "8/2p1p1p1/8/8/8/8/2P1P1P1/8 w", .moves = {.pawns = 6}}};
 
-    Result run() override {
-        generate_pawn_moves();
+    // initialize boards
+    auto boards = cases |
+        std::ranges::views::transform(
+            [&](const PositionTestCase& position_case) {
+                return Board::parse_fen_repr(position_case.fen);
+            });
 
-        uint8_t produced = get_generated_moves().size();
-        bool passed = produced == expected;
+    // generate moves for every position
+    auto generation_results = boards |
+        std::ranges::views::transform(
+            [&](auto board) { return MoveGenerator::generate_moves(board); });
 
-        return Result{.title = "Pawn generation logic test",
-                      .passed = passed,
-                      .expected = expected,
-                      .produced = produced};
+    // test generation for each position
+    for (auto [moves, position_case] :
+        std::ranges::zip_view(generation_results, cases)) {
+        MoveCounts result_count;
+
+        result_count.pawns = std::ranges::count_if(
+            moves, [](Move move) { return move.moved == Piece::PAWNS; });
+
+        bool passed = position_case.moves.pawns == result_count.pawns;
+
+        if (passed && pass_silently) continue;
+
+        std::println("{}",
+            message("Pawn Count Generation Test",
+                std::format("FEN: {}", position_case.fen),
+                position_case.moves.pawns, result_count.pawns, passed));
     }
-};
+}
 
 }  // namespace core::test
 
-int main() {
-    auto position1 = core::Board::parse_fen_repr(
-        "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+int main(int argc, char* argv[]) {
+    bool pass_silenty = false;
 
-    std::print("{}", core::test::PawnGenTest(position1, 16).run().message());
+    for (int i = 0; i < argc; ++i) {
+        std::string arg = argv[i];
+
+        if (arg == "--pass-silently") pass_silenty = true;
+    }
+
+    core::test::TestGenerationCount(pass_silenty);
+    return 0;
 }
